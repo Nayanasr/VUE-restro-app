d5a383d6b8149f3d87c7f5e65e5c888ecf3b98ed
Nayana Ramesh <107012@tfcorp.com>
2022-09-22 20:02:30 +0530

    vue 2nd project added

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>Little Indian Restro</h1>
+  <ul>
+    <dish-details
+      v-for="dish in dishes"
+      :key="dish.id"
+      :id="dish.id"
+      :name="dish.name"
+      :description="dish.description"
+      :price="dish.price"
+      :isFavourite="dish.isFavourite"
+      @toggleFavouriteStatus="toggleFavouriteStatus"
+    />
+  </ul>
+</template>
+
+<script>
+import DishDetails from "./components/DishDetails.vue";
+export default {
+  name: "App",
+  components: { DishDetails },
+  data() {
+    return {
+      dishes: [
+        {
+          id: "avi",
+          name: "Butter Chicken",
+          description: "It's finger licking good",
+          price: 350,
+          isFavourite: true,
+        },
+        {
+          id: "vaibhav",
+          name: "Chole Bhature",
+          description: "The crispy puffed bhatura and gravy",
+          price: 150,
+          isFavourite: false,
+        },
+        {
+          id: "akshata",
+          name: "Fish Fry",
+          description: "includes french fries,coleslaw",
+          price: 300,
+          isFavourite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    //to get dishDetails from child component
+    toggleFavouriteStatus(dishID) {
+      // console.log("dish ID in parent", dishID);
+      let identifiedDish = this.dishes.find((dish) => dish.id === dishID);
+      identifiedDish.isFavourite = !identifiedDish.isFavourite;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app h1 {
+  background-color: rgb(226, 43, 211);
+}
+</style>
---
+++ src/components/DishDetails.vue
@@ -0,0 +1,68 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavourite ? "(Favourite)" : "" }}</h2>
+    <button @click="toggleFavourite">Toggle Favourite</button>
+    <button @click="toggleDetails">
+      {{ displayDetails ? "Hide" : "Show" }}Details
+    </button>
+    <ul v-if="displayDetails">
+      <li><strong>Description:</strong>{{ description }}</li>
+      <li><strong>Price:</strong>{{ price }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  emits: ["toggleFavouriteStatus"],
+  //   props: ["name", "description", "price", "isFavourite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      //   dishIsFavourite: this.isFavourite,
+      displayDetails: false,
+    };
+  },
+  methods: {
+    toggleFavourite() {
+      //   if (this.dishIsFavourite) {
+      //     this.dishIsFavourite = false;
+      //   } else {
+      //     this.dishIsFavourite = true;
+      //   }
+      //   this.dishIsFavourite = !this.dishIsFavourite;
+      this.$emit("toggleFavouriteStatus", this.id);
+    },
+    toggleDetails() {
+      this.displayDetails = !this.displayDetails;
+    },
+  },
+};
+</script>
+
+<style>
+li {
+  list-style-type: none;
+}
+</style>
